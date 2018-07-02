ce4a23d0bad1decf9e7069ee866358aa13e1154c
verascity <svsticker@gmail.com>
2018-07-02 14:33:13 -0500

    formatting

--- SQL/left-right.sql
+++ SQL/left-right.sql
@@ -1,10 +1,12 @@
 --In the accounts table, there is a column holding the website for each company. Pull the website extensions and provide how many of each website type exist in the accounts table.
-SELECT RIGHT(website, 3) AS extension, COUNT(website) AS count
+SELECT RIGHT(website, 3) AS extension, 
+    COUNT(website) AS count
 FROM accounts
 GROUP BY 1;
 
 --Use the accounts table to pull the first letter of each company name to see the distribution of company names that begin with each letter (or number). 
-SELECT LEFT(name, 1) AS first_letter, COUNT(name) AS count
+SELECT LEFT(name, 1) AS first_letter, 
+    COUNT(name) AS count
 FROM accounts
 GROUP BY 1
 ORDER BY 1;
@@ -13,7 +15,8 @@ ORDER BY 1;
 SELECT CASE
     WHEN UPPER(LEFT(name, 1)) BETWEEN 'A' AND 'Z' THEN 'letter'
     WHEN LEFT(name, 1) BETWEEN '1' AND '9' THEN 'number'
-    END first_char, COUNT(name) counts
+    END first_char, 
+    COUNT(name) counts
 FROM accounts
 GROUP BY 1;
 
@@ -21,6 +24,7 @@ GROUP BY 1;
 SELECT CASE
     WHEN UPPER(LEFT(name, 1)) IN ('A', 'E', 'I', 'O', 'U') THEN 'vowel'
     ELSE 'not a vowel'
-    END first_char, COUNT(name) counts
+    END first_char, 
+    COUNT(name) counts
 FROM accounts
 GROUP BY 1;
