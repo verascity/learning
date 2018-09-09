c95ec3f0a616de091b3d7e692ed144329c5a3af4
verascity <svsticker@gmail.com>
2018-09-09 16:16:48 -0400

    backfill missing value

--- Udacity Data Analyst Nanodegree/Exploring Weather Trends/weather_trends.py
+++ Udacity Data Analyst Nanodegree/Exploring Weather Trends/weather_trends.py
@@ -3,12 +3,18 @@
 Created on Sun Sep  9 13:48:04 2018
 
 This script analyzes weather_data.csv to look for trends in yearly average
-temperature, both globally and locally to New York City, from 1950 to 2013. 
+temperature, both globally and locally to New York City, from 1750 to 2013. 
 """
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 weather = pd.read_csv("weather_data.csv")
-print(weather.describe())
+
+#print(weather.describe()) #Describe reveals one missing value in ny_temp.
+#print(weather[weather['ny_temp'].isna()]) #Reveals missing year is 1780
+weather = weather.bfill() #The ny_temp for 1779 is an outlier at 0.25, so backfilling makes the most sense.
+
+
