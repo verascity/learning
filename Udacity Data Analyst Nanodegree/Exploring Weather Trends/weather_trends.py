03af253e430c81a087bbc9596c5b85a06b1221db
verascity <svsticker@gmail.com>
2018-09-09 19:46:27 -0400

    group data

--- Udacity Data Analyst Nanodegree/Exploring Weather Trends/weather_trends.py
+++ Udacity Data Analyst Nanodegree/Exploring Weather Trends/weather_trends.py
@@ -19,7 +19,7 @@ weather = weather.bfill() #The ny_temp for 1779 is an outlier at 0.25, so backfi
 #Calculate 10-year moving averages:
 weather["ny_mov_avg"] = weather["ny_temp"].rolling(window=10).mean() 
 weather["global_mov_avg"] = weather["global_temp"].rolling(window=10).mean()
-
+"""
 #Plot the data:
 plt.figure(figsize=(10, 5)) 
 plt.plot(weather["year"], weather["ny_mov_avg"], 'orange', weather["year"], weather["global_mov_avg"], 'blue')
@@ -31,6 +31,7 @@ plt.legend(["New York", "Global"])
 #Calculate the correlation coefficient for the two moving averages:
 corr = weather["ny_mov_avg"].corr(weather["global_mov_avg"]) 
 print(corr)
+"""
 
 """
 After looking at this data, I was curious about how other cities would
@@ -38,4 +39,9 @@ compare, so I generated a new csv with a random sample of five cities.
 """
 
 weather2 = pd.read_csv("weather_data2.csv")
-print(weather2.describe())
+weather2["location"] = weather2["city"] + ", " + weather2["country"]
+weather2 = weather2[["year", "location", "city_temp", "global_temp"]]
+
+grouped = weather2["city_temp"].groupby([weather2["location"], weather2["year"]])
+print(grouped.mean())
+    
