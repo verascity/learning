411b38f52e413281766eb3c7ea373d1e2f25a636
Vera Sticker <svsticker@gmail.com>
2018-10-08 23:54:34 -0400

    spellcheck

--- Udacity Data Analyst Nanodegree/Investigate a Dataset/noshows.py
+++ Udacity Data Analyst Nanodegree/Investigate a Dataset/noshows.py
@@ -17,12 +17,15 @@ For the sake of decluttering, I'm going to reset the index to the
 AppointmentID, which is unique to every row and can serve as the primary
 key. There are no rows with missing data, so this shouldn't pose a problem.
 
-I'm also going to drop and add some columns. I know that neighbourhood does
+I'm also going to edit the columns. I know that neighbourhood does
 not interest me, so I'm going to drop that. I am interested in repeat no-shows,
 so I'm going to create a new column to indicate duplicates, then drop the
-very unwieldy 'PatientId' column.
+very unwieldy 'PatientId' column. Finally, I'm going to fix some mispelled
+column names.
 """
 
 df = df.set_index('AppointmentID')
 df['Duplicate'] = df['PatientId'].duplicated()
 df = df.drop(['PatientId', 'Neighbourhood'], axis=1)
+df = df.rename(index=str, columns={"Hipertension":"Hypertension", 
+                                   "Handcap" : "Disability"})
