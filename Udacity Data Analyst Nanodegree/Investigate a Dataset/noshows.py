91cf7b506520ec219898c0b374944687f0c588f6
Vera Sticker <svsticker@gmail.com>
2018-10-09 14:39:48 -0400

    pretty up plots

--- Udacity Data Analyst Nanodegree/Investigate a Dataset/noshows.py
+++ Udacity Data Analyst Nanodegree/Investigate a Dataset/noshows.py
@@ -7,6 +7,7 @@ containing information about no-shows to medical appointments in Brazil.
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 pd.options.display.max_columns = None
 
@@ -31,7 +32,7 @@ column for time differences.
 
 Finally, after looking through the data, I realized there are some wonky values 
 in the 'Age' and new 'TimeBetween' columns, so I'll also restrict those to 
-avoid outliers.
+avoid extreme outliers.
 """
 
 df = df.set_index('AppointmentID')
@@ -44,7 +45,7 @@ df['AppointmentDay'] = pd.to_datetime(df['AppointmentDay'], infer_datetime_forma
 df['ScheduledDay'] = pd.to_datetime(df['ScheduledDay'], infer_datetime_format=True).dt.date
 df['TimeBetween'] = (df['AppointmentDay'] - df['ScheduledDay']).dt.days
 df = df.drop(['AppointmentDay', 'ScheduledDay', 'PatientId', 'Neighbourhood'], axis=1)
-df = df[df['TimeBetween'] >= 0]
+df = df[(df['TimeBetween'] >= 0) & (df['TimeBetween'] <= 100)]
 
 
 """
@@ -58,14 +59,28 @@ timebetween, with a slight possibility of association with SMS_received.
 
 noshows = df.groupby('No-show').mean()
 #print(noshows) # There aren't a lot of strong associations!
-
+#print(df.describe())
 
 noshows = noshows[['Age', 'TimeBetween', 'SMS_received']]
 fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8, 10))
 x_val = noshows.index
-ax1.bar(x_val, noshows['Age'])
-ax2.bar(x_val, noshows['TimeBetween'])
-ax3.bar(x_val, noshows['SMS_received'])
+ax1.bar(x_val, noshows['Age'], color='brown')
+ax1.set_ylim([0,50])
+ax1.set_ylabel('Age in Years')
+ax1.tick_params(axis='both', length=0)
+ax2.bar(x_val, noshows['TimeBetween'], color='green')
+ax2.set_ylim([0,30])
+ax2.set_ylabel('Time Btw Schd/Appt in Days')
+ax2.tick_params(axis='both', length=0)
+ax3.bar(x_val, noshows['SMS_received'], color='purple')
+ax3.set_ylim([0,1])
+ax3.set_ylabel('Probability of SMS Rcvd')
+ax3.tick_params(axis='both', length=0)
+sns.despine(top=True)
+
+
+
+
 
 
 """
