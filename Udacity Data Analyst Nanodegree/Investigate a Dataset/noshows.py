6a964906d264ff3e53a9d69b7333cc53a4cb752c
Vera Sticker <svsticker@gmail.com>
2018-10-09 00:19:25 -0400

    wrangle ages

--- Udacity Data Analyst Nanodegree/Investigate a Dataset/noshows.py
+++ Udacity Data Analyst Nanodegree/Investigate a Dataset/noshows.py
@@ -9,9 +9,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+pd.options.display.max_columns = None
+
 df = pd.read_csv('noshowappointments.csv')
 
 """
+Data Cleaning/Wrangling:
+    
 For the sake of decluttering, I'm going to reset the index to the
 AppointmentID, which is unique to every row and can serve as the primary
 key. There are no rows with missing data, so this shouldn't pose a problem.
@@ -21,6 +25,9 @@ not interest me, so I'm going to drop that. I am interested in repeat no-shows,
 so I'm going to create a new column to indicate duplicates, then drop the 
 unwieldy 'PatientId' column. Finally, I'm going to fix some mispelled 
 column names.
+
+After looking through some data, I realized there are some wonky values, so
+I'll also restrict those data to avoid outliers.
 """
 
 df = df.set_index('AppointmentID')
@@ -28,10 +35,21 @@ df['Duplicate'] = df['PatientId'].duplicated()
 df = df.drop(['PatientId', 'Neighbourhood'], axis=1)
 df = df.rename(index=str, columns={"Hipertension":"Hypertension", 
                                    "Handcap" : "Disability"})
+df = df[(df['Age'] >= 0) & (df['Age'] <= 95)]
 
 """
-Question 1: What features are associated with repeat no-shows?
+Question 1: What features are associated with no-shows, generally?
 """
 
-repeats = df[df['No-show'] == 'Yes'].groupby('Duplicate')
-print(repeats.head())
+noshows = df.groupby('No-show').mean()
+#print(noshows) # There aren't a lot of strong associations!
+print(df.describe())
+
+
+
+
+"""
+Question 2: What features are associated with repeat no-shows?
+"""
+
+#repeats = df[df['No-show'] == 'Yes'].groupby('Duplicate')
