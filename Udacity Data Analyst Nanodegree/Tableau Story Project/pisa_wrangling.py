e4779265f558818f01ddd70dfb9eca8900439dea
Vera Sticker <svsticker@gmail.com>
2018-12-25 14:03:23 -0500

    select columns to use

--- Udacity Data Analyst Nanodegree/Tableau Story Project/pisa_wrangling.py
+++ Udacity Data Analyst Nanodegree/Tableau Story Project/pisa_wrangling.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
+import numpy as np
 
-#pisa = pd.read_csv('pisa2012.csv', encoding='cp1252')
-#pisa_dict = pd.read_csv('pisadict2012.csv', encoding='cp1252')
+pisa = pd.read_csv('pisa2012.csv', encoding='cp1252')
+pisa_dict = pd.read_csv('pisadict2012.csv', encoding='cp1252')
 
 
 new_pisa_names = ['']
@@ -29,4 +30,42 @@ countries = {
 }
 
 pisa = pisa.replace(countries)
+
+final_cols = ['Country code 3-character', 'OECD country', 'International Grade', 
+              'Gender', 'Attend <ISCED 0>', 'Mother<Highest Schooling>', 
+              'Father<Highest Schooling>', 'Country of Birth International - Self',
+              'International Language at Home', 
+              'Age started learning <test language>',
+              'Learning time (minutes per week)  - <test language>',
+              'Index of economic, social and cultural status',
+              'Math Anxiety - Worry That It Will Be Difficult',
+              'Math Anxiety - Get Very Nervous', 
+              'Math Anxiety - Feel Helpless', 
+              'Math Anxiety - Worry About Getting Poor <Grades>',
+              'Math Self-Concept - Not Good at Maths', 
+              'Math Self-Concept- Get Good <Grades>',
+              'Math Self-Concept - Learn Quickly', 
+              'Math Self-Concept - Understand Difficult Work', 
+              'Math Interest - Look Forward to Lessons', 
+              'Math Interest - Enjoy Maths',
+              'Instrumental Motivation - Worthwhile for Work',
+              'Instrumental Motivation - Worthwhile for Career Chances', 
+              'Instrumental Motivation - Important for Future Study', 
+              'Instrumental Motivation - Helps to Get a Job',
+              'Subjective Norms -Friends Do Well in Mathematics', 
+              'Subjective Norms - Friends Enjoy Mathematics Tests', 
+              'Subjective Norms - Parents Believe Studying Mathematics Is Important',
+              'Subjective Norms - Parents Like Mathematics',
+              'Math Self-Efficacy - Using a <Train Timetable>',
+              'Math Self-Efficacy - Calculating TV Discount',
+              'Math Self-Efficacy - Calculating Square Metres of Tiles',
+              'Math Self-Efficacy - Understanding Graphs in Newspapers',
+              'Plausible value 1 in mathematics',
+              'Plausible value 2 in mathematics',
+              'Plausible value 3 in mathematics',
+              'Plausible value 4 in mathematics',
+              'Plausible value 5 in mathematics',
+              'FINAL STUDENT WEIGHT']
+
+pisa_final = pisa[final_cols]
 
