0399b2398d13c550101116a8ff6667e28bf23caa
Vera Sticker <svsticker@gmail.com>
2018-12-25 12:52:25 -0500

    fix country codes

--- Udacity Data Analyst Nanodegree/Tableau Story Project/pisa_wrangling.py
+++ Udacity Data Analyst Nanodegree/Tableau Story Project/pisa_wrangling.py
@@ -2,8 +2,8 @@
 
 import pandas as pd
 
-pisa = pd.read_csv('pisa2012.csv', encoding='cp1252')
-pisa_dict = pd.read_csv('pisadict2012.csv', encoding='cp1252')
+#pisa = pd.read_csv('pisa2012.csv', encoding='cp1252')
+#pisa_dict = pd.read_csv('pisadict2012.csv', encoding='cp1252')
 
 
 new_pisa_names = ['']
@@ -12,4 +12,21 @@ for row in pisa_dict['x']:
     
 pisa.columns = new_pisa_names
 
-print(pisa.head())
+countries = {
+    'Country code 3-character': {
+        'China-Shanghai': 'China',
+        'Chinese Taipei': 'Taiwan',
+        'Connecticut (USA)': 'United States',
+        'Florida (USA)': 'United States',
+        'Hong Kong-China': 'China',
+        'Korea': 'South Korea',
+        'Macao-China': 'China',
+        'Massachusetts (USA)': 'United States',
+        'Perm(Russian Federation)': 'Russia',
+        'Russian Federation': 'Russia',
+        'United States of America': 'United States'
+    }    
+}
+
+pisa = pisa.replace(countries)
+
