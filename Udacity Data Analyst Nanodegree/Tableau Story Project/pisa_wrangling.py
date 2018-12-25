578d0ed586a1ada170ba5f29b7669f83cd9a9e5e
Vera Sticker <svsticker@gmail.com>
2018-12-25 16:11:30 -0500

    create new csv

--- Udacity Data Analyst Nanodegree/Tableau Story Project/pisa_wrangling.py
+++ Udacity Data Analyst Nanodegree/Tableau Story Project/pisa_wrangling.py
@@ -2,33 +2,33 @@
 
 import pandas as pd
 
-#pisa = pd.read_csv('pisa2012.csv', encoding='cp1252')
-#pisa_dict = pd.read_csv('pisadict2012.csv', encoding='cp1252')
-#
-#
-#new_pisa_names = [''] 
-#for row in pisa_dict['x']:
-#    new_pisa_names.append(row)
-#    
-#pisa.columns = new_pisa_names
-#
-#countries = {
-#    'Country code 3-character': {
-#        'China-Shanghai': 'China',
-#        'Chinese Taipei': 'Taiwan',
-#        'Connecticut (USA)': 'United States',
-#        'Florida (USA)': 'United States',
-#        'Hong Kong-China': 'China',
-#        'Korea': 'South Korea',
-#        'Macao-China': 'China',
-#        'Massachusetts (USA)': 'United States',
-#        'Perm(Russian Federation)': 'Russia',
-#        'Russian Federation': 'Russia',
-#        'United States of America': 'United States'
-#    }    
-#}
-#
-#pisa = pisa.replace(countries)
+pisa = pd.read_csv('pisa2012.csv', encoding='cp1252')
+pisa_dict = pd.read_csv('pisadict2012.csv', encoding='cp1252')
+
+
+new_pisa_names = [''] 
+for row in pisa_dict['x']:
+    new_pisa_names.append(row)
+    
+pisa.columns = new_pisa_names
+
+countries = {
+    'Country code 3-character': {
+        'China-Shanghai': 'China',
+        'Chinese Taipei': 'Taiwan',
+        'Connecticut (USA)': 'United States',
+        'Florida (USA)': 'United States',
+        'Hong Kong-China': 'China',
+        'Korea': 'South Korea',
+        'Macao-China': 'China',
+        'Massachusetts (USA)': 'United States',
+        'Perm(Russian Federation)': 'Russia',
+        'Russian Federation': 'Russia',
+        'United States of America': 'United States'
+    }    
+}
+
+pisa = pisa.replace(countries)
 
 final_cols = ['Country code 3-character', 'OECD country', 'International Grade', 
               'Gender', 'Attend <ISCED 0>', 'Age at <ISCED 1>', 'Mother<Highest Schooling>', 
@@ -99,3 +99,4 @@ pisa_final = pisa_final[pisa_final['International Grade'] <= 12] # Cleaning up s
 
 pisa_final = pisa_final[pisa_final['Age At School Start'] <= 13] # Cleaning up some incongruent-seeming ages
 
+pisa_final.to_csv('pisa_final.csv', chunksize=20000, index=False)
