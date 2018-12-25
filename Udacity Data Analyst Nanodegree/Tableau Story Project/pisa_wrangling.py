e20e46e39ec0decf6501f6850865589fc5a6e4a0
Vera Sticker <svsticker@gmail.com>
2018-12-25 16:00:45 -0500

    clean invalid values

--- Udacity Data Analyst Nanodegree/Tableau Story Project/pisa_wrangling.py
+++ Udacity Data Analyst Nanodegree/Tableau Story Project/pisa_wrangling.py
@@ -1,38 +1,37 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-import numpy as np
 
-pisa = pd.read_csv('pisa2012.csv', encoding='cp1252')
-pisa_dict = pd.read_csv('pisadict2012.csv', encoding='cp1252')
-
-
-new_pisa_names = ['']
-for row in pisa_dict['x']:
-    new_pisa_names.append(row)
-    
-pisa.columns = new_pisa_names
-
-countries = {
-    'Country code 3-character': {
-        'China-Shanghai': 'China',
-        'Chinese Taipei': 'Taiwan',
-        'Connecticut (USA)': 'United States',
-        'Florida (USA)': 'United States',
-        'Hong Kong-China': 'China',
-        'Korea': 'South Korea',
-        'Macao-China': 'China',
-        'Massachusetts (USA)': 'United States',
-        'Perm(Russian Federation)': 'Russia',
-        'Russian Federation': 'Russia',
-        'United States of America': 'United States'
-    }    
-}
-
-pisa = pisa.replace(countries)
+#pisa = pd.read_csv('pisa2012.csv', encoding='cp1252')
+#pisa_dict = pd.read_csv('pisadict2012.csv', encoding='cp1252')
+#
+#
+#new_pisa_names = [''] 
+#for row in pisa_dict['x']:
+#    new_pisa_names.append(row)
+#    
+#pisa.columns = new_pisa_names
+#
+#countries = {
+#    'Country code 3-character': {
+#        'China-Shanghai': 'China',
+#        'Chinese Taipei': 'Taiwan',
+#        'Connecticut (USA)': 'United States',
+#        'Florida (USA)': 'United States',
+#        'Hong Kong-China': 'China',
+#        'Korea': 'South Korea',
+#        'Macao-China': 'China',
+#        'Massachusetts (USA)': 'United States',
+#        'Perm(Russian Federation)': 'Russia',
+#        'Russian Federation': 'Russia',
+#        'United States of America': 'United States'
+#    }    
+#}
+#
+#pisa = pisa.replace(countries)
 
 final_cols = ['Country code 3-character', 'OECD country', 'International Grade', 
-              'Gender', 'Attend <ISCED 0>', 'Mother<Highest Schooling>', 
+              'Gender', 'Attend <ISCED 0>', 'Age at <ISCED 1>', 'Mother<Highest Schooling>', 
               'Father<Highest Schooling>', 'Country of Birth International - Self',
               'International Language at Home', 
               'Age started learning <test language>',
@@ -66,7 +65,7 @@ final_cols = ['Country code 3-character', 'OECD country', 'International Grade',
 pisa_final = pisa[final_cols]
 
 clean_col_names = ['Country', 'OECD', 'International Grade', 'Gender',
-                   'Attended Pre-Primary', 'Mother Highest Schooling',
+                   'Attended Pre-Primary', 'Age At School Start', 'Mother Highest Schooling',
                    'Father Highest Schooling', 'Birth Country', 'Home Language',
                    'Age Started Learning Test Language', 'Test Language Learning Time',
                    'ESCS Index', 'Math Anxiety - Worry That It Will Be Difficult',
@@ -96,4 +95,7 @@ clean_col_names = ['Country', 'OECD', 'International Grade', 'Gender',
 
 pisa_final.columns = clean_col_names
 
-print(pisa_final.columns)
+pisa_final = pisa_final[pisa_final['International Grade'] <= 12] # Cleaning up some weird values that don't detract from the analysis
+
+pisa_final = pisa_final[pisa_final['Age At School Start'] <= 13] # Cleaning up some incongruent-seeming ages
+
